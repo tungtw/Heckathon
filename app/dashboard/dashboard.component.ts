9331f900a65edfecf19d85a016db0196b11c304c
WT <abc@cde.com>
2016-07-17 08:57:45 -0500

    Layout

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {provideRouter, RouterConfig, ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
     moduleId: module.id,
     selector: 'selector',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
+    styleUrls: ['./dashboard.component.css'],
+    directives: [ROUTER_DIRECTIVES]
 })
 export class Dashboard implements OnInit {
     constructor() { }
 
     ngOnInit() { }
+
+    divClick(){
+        alert("divClick!");
+    }
+
+    btnClick(){
+        alert("btnClick!");
+    }
+
+    typeBtnClick(){
+        alert("typeBtnClick!");
+    }
+
+    returnBool()
+    {
+        return true;
+    }
 
 }
