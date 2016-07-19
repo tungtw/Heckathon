2ff1b0a8ebf80f08ad7ad2043dd8c8a18386701f
WT <abc@cde.com>
2016-07-19 12:04:57 -0500

    temp

--- app/ng2-template-syntax/template-syntax.component.ts
+++ app/ng2-template-syntax/template-syntax.component.ts
@@ -4,12 +4,13 @@ import {Hero} from "../heroes/hero.service";
 @Component({
   moduleId: module.id,
   selector: 'selector',
-  templateUrl: './template-syntax.component.html'
+  templateUrl: './template-syntax.component.html',
+  styleUrls: ['./template-syntax.component.css']
 })
 export class TemplateSyntax implements OnInit {
   currentHero:Hero;
   isGood:boolean;
-
+  isSpecialCSS:boolean;
   constructor() {
 
   }
@@ -17,6 +18,7 @@ export class TemplateSyntax implements OnInit {
   ngOnInit() {
     this.currentHero = new Hero(0, "Wen Tung");
     this.isGood = true;
+    this.isSpecialCSS = true;
   }
 
   getVal() {
