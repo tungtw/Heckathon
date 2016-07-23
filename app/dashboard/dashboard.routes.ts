615e3f4a82e8d424b51407a6664d31c2b4bde0b6
WT <abc@cde.com>
2016-07-23 15:01:57 -0500

    empty chart

--- app/dashboard/dashboard.routes.ts
+++ app/dashboard/dashboard.routes.ts
@@ -1,34 +1,33 @@
-import { RouterConfig }          from '@angular/router';
-import { Dashboard }          from './dashboard.component';
-export const dashboardRoutes: RouterConfig = [
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: 'dash-head',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: 'dashboard',
-    component: Dashboard,
-    children: [
-    //   {
-    //     path: 'admin',
-    //     component: CrisisAdminComponent,
-    //     canActivate: [AuthGuard]
-    //   },
-    //   {
-    //     path: ':id',
-    //     component: CrisisDetailComponent,
-    //     canDeactivate: [CanDeactivateGuard]
-    //   },
-    //   {
-    //     path: '',
-    //     component: CrisisListComponent
-    //   }
-    ]
-  }
+import {RouterConfig}          from '@angular/router';
+import {Dashboard}          from './dashboard.component';
+import {ChartComponent} from "../pages/charts/components/charts";
+export const dashboardRoutes:RouterConfig = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard,
+        children: [
+            {
+                path: 'chart',
+                component: ChartComponent
+            },
+            // {
+            //   path: ':id',
+            //   component: CrisisDetailComponent,
+            //   canDeactivate: [CanDeactivateGuard]
+            // },
+            // {
+            //   path: '',
+            //   component: CrisisListComponent
+            // }
+        ]
+    }
 ];
