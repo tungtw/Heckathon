e0d8be88b54d1b4d2db44b392048a66534bfc37d
WT <abc@cde.com>
2016-07-26 13:24:18 -0500

    http test local data 2

--- app/pages/charts/charts.component.ts
+++ app/pages/charts/charts.component.ts
@@ -1,8 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ROUTER_DIRECTIVES, ActivatedRoute} from '@angular/router';
 import {UIChart, Message} from 'primeng/primeng';
 import {ChartDataService} from "../../shared/services/chart-data.service";
-import {ChartData} from "../../shared/data/chart-data";
 import {TestService} from "../../shared/services/test.service";
 
 @Component({
@@ -13,17 +12,15 @@ import {TestService} from "../../shared/services/test.service";
     providers: [ChartDataService, TestService]
 })
 
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, OnDestroy {
 
     chartId:number;
     chartData:any;
     errorMessage:any;
 
     private sub:any;
-    data:any;
     msgs:Message[];
 
-    //Observable<string>;
     constructor(private route:ActivatedRoute, private chartDataService:TestService) {
         this.route.params.subscribe(params => {
             this.chartId = +params['id'];
@@ -36,16 +33,10 @@ export class ChartComponent implements OnInit {
             this.chartDataService.getChartData(this.chartId).subscribe(
                 data=>this.chartData = data,
                 error=>this.errorMessage = <any>error);
-            // this.chartData = this.chartDataService.getChartData(this.chartId);
         });
-
-        // this.sub = this.route.params.subscribe(params => {
-        //     this.chartId = +params['id'];
-        //     this.chartDataService.getChartData(this.chartId)
-        //         .then((data:any)=> {
-        //             this.chartData = data;
-        //         });
-        // });
+    }
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 
     selectData(event:any) {
@@ -53,8 +44,7 @@ export class ChartComponent implements OnInit {
         this.msgs.push({
             severity: 'info',
             summary: 'Data Selected',
-            'detail': this.data.datasets[event.element._datasetIndex].data[event.element._index]
+            'detail': this.chartData.datasets[event.element._datasetIndex].data[event.element._index]
         });
     }
-
 }
