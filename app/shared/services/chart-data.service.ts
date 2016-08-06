237ebbd0db5bd1e7cd0abf7120597351737436a8
WT <abc@cde.com>
2016-08-06 08:06:49 -0500

    prior to R

--- app/shared/services/chart-data.service.ts
+++ app/shared/services/chart-data.service.ts
@@ -5,24 +5,24 @@ import {ChartData} from "../data/chart-data";
 
 @Injectable()
 export class ChartDataService {
-    private chartData1Url = 'app/shared/data/chart-data1.json';  // URL to web API
-    private chartData2Url = 'http://localhost:11992/api/ChartData';  // URL to web API
-    constructor(private http:Http) {
+    // private chartData1Url = 'app/shared/data/chart-data1.json';  // URL to web API
+    private chartDataBaseUrl = 'http://localhost:11992/api/ChartData';  // URL to web API
+    constructor(private http: Http) {
     }
 
-    getChartData(chartNo:number):Observable<ChartData[]> {
-        let chartDataUrl = this.chartData2Url + '/' + chartNo;//id == 1 ? this.chartData1Url : this.chartData2Url;
+    getChartData(chartNo: number): Observable<ChartData[]> {
+        let chartDataUrl = this.chartDataBaseUrl + '/' + chartNo;//id == 1 ? this.chartData1Url : this.chartDataBaseUrl;
         return this.http.get(chartDataUrl)
-            .map(ChartDataService.extractData)
+            .map((res: Response) => ChartDataService.extractData(res))
             .catch(ChartDataService.handleError);
     }
 
-    private static extractData(res:Response) {
+    private static extractData(res: Response) {
         let body = res.json(); //parse the response data into a JSON object
         return body.data || {};
     }
 
-    private static handleError(error:any) {
+    private static handleError(error: any) {
         let errMsg = (error.message) ? error.message :
             error.status ? `${error.status} - ${error.statusText}` : 'Server error';
         console.error(errMsg); // log to console instead
